e75f42a0c98ffdd61ec319019959c7d0c9f89762
Bryan Lin <bryanlin39@gmail.com>
2017-03-31 08:54:02 -0700

    add input form and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  
+  $("form#number").submit(function(event) {
+    event.preventDefault();
+
+    var numberInput = parseInt($("#number-input").val());
+    
+  });
 });
